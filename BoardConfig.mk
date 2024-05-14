e64f758235b878ee6567e2e81fb4fa169c169e48
MisterZtr <andey2004586@gmail.com>
2024-05-14 18:47:20 +0300

    shark8: Unnecessary items removed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 #Perhaps this value is not correct
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone1/temp"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone25/temp"
 
 # Debug
 TARGET_USES_LOGD := true
